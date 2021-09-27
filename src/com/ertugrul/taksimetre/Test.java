e6163876fb8de8dfc074a51e43377920b3c58d6f
Ertuğrul Gürkan <ertugrulgurkan16@gmail.com>
2021-09-27 16:00:27 +0300

    third commit

---
+++ src/com/ertugrul/taksimetre/Test.java
@@ -0,0 +1,20 @@
+package com.ertugrul.taksimetre;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        int km;
+        double perKm=2.20,total,startPrice=10;
+
+        Scanner input= new Scanner(System.in);
+        System.out.println("Mesafeyi km cinsinden giriniz : ");
+        km=input.nextInt();
+
+        total=km*perKm;
+        total+=startPrice;
+
+        total=(total<20)?20:total;
+        System.out.println("Toplam tutar : "+total);
+    }
+}
