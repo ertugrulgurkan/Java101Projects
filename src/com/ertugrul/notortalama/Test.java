401d14af80e67f927cb086777058a3f67ac0ee53
Ertuğrul Gürkan <ertugrulgurkan16@gmail.com>
2021-09-27 15:52:25 +0300

    Beginner - Java 101 exercises for patika.dev tasks

---
+++ src/com/ertugrul/notortalama/Test.java
@@ -0,0 +1,33 @@
+package com.ertugrul.notortalama;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Matematik notunuzu giriniz :");
+        int matematik = input.nextInt();
+
+        System.out.println("Fizik notunuzu giriniz :");
+        int fizik = input.nextInt();
+
+        System.out.println("Türkçe notunuzu giriniz :");
+        int turkce = input.nextInt();
+
+        System.out.println("Tarih notunuzu giriniz :");
+        int tarih = input.nextInt();
+
+        System.out.println("Müzik notunuzu giriniz :");
+        int muzik = input.nextInt();
+
+        System.out.println("Kimya notunuzu giriniz :");
+        int kimya = input.nextInt();
+
+        int toplam = (matematik+fizik+turkce+tarih+muzik+kimya);
+        double sonuc = toplam / 6.0;
+        System.out.println("Not ortalaması : " + sonuc);
+
+        String gectiKaldi = sonuc >= 60.0 ? "Geçti":"Kaldı";
+        System.out.println(gectiKaldi);
+    }
+}
