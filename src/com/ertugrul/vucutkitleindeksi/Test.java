52eaaac2cc2dd2509975516d710fc24ef53636be
Ertuğrul Gürkan <ertugrulgurkan16@gmail.com>
2021-09-27 16:03:44 +0300

    vki added

---
+++ src/com/ertugrul/vucutkitleindeksi/Test.java
@@ -0,0 +1,16 @@
+package com.ertugrul.vucutkitleindeksi;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        double boy,kilo,sonuc;
+        System.out.print("Lütfen boyunuzu metre cinsinden giriniz : ");
+        boy=scanner.nextDouble();
+        System.out.println("\nLütgen kilonuzu giriniz : ");
+        kilo=scanner.nextDouble();
+        sonuc=kilo/(boy*boy);
+        System.out.println("sonucunuz : "+sonuc);
+    }
+}
